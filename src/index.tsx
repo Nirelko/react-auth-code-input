975abbb45cc52d97f270f035b2c2e440b668fd48
Nirel Yehoyada <Nirelko1@gmail.com>
2021-05-11 16:55:52 +0300

    removed the redundant pattern property

--- src/index.tsx
+++ src/index.tsx
@@ -80,7 +80,6 @@ const AuthCode: React.FC<Props> = ({
     inputs.push(
       <input
         key={i}
-        pattern={allowedCharacters?.toString()}
         onChange={handleOnChange}
         onKeyDown={handleOnKeyDown}
         onFocus={handleOnFocus}
